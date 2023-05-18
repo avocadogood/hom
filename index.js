1bde02cdb69a48a138b24e2061e34b40a43ab499
avocadogood <132554456+avocadogood@users.noreply.github.com>
2023-05-18 17:10:45 +0900

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-window.onload = function(){
-    // button=document.getElementById("btn");
-    clock=document.getElementById("clock");
-    setInterval(x, 1000);
+function padZero(number) {
+  // number가 10보다 작으면 앞에 0을 추가하여 두 자릿수로 표시
+  return (number < 10) ? '0' + number : number;
 }
 
 function x() {
   var now = new Date();
   var result =
-      now.getFullYear() + ". "
-      + (now.getMonth() + 1) + ". "
-      + now.getDate() + ". "
-      + now.getHours() + ":"
-      + now.getMinutes() + ":"
-      + now.getSeconds();
-  clock.innerHTML = result;
+  now.getFullYear() + ". "
+      + padZero(now.getMonth() + 1) + ". "
+      + padZero(now.getDate()) + ". "
+      + padZero(now.getHours()) + ":"
+      + padZero(now.getMinutes()) + ":"
+      + padZero(now.getSeconds());
+      clock.innerHTML = result;
+}
+
+
+window.onload = function(){
+  // button=document.getElementById("btn");
+  clock=document.getElementById("clock");
+  setInterval(x, 1000);
 }
